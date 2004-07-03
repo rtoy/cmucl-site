e05e45cf1b8edd4aa4185f69f09414cff0fd329b
emarsden <emarsden>
2004-07-03 15:05:33 +0000

    Web site:
      - make links to the documentation refer to a copy of the docs
        that resides on common-lisp.net/project/cmucl/public_html/docs.
        This makes the documentation accessible by HTTP.

--- cmucl-www/build.el
+++ cmucl-www/build.el
@@ -31,7 +31,8 @@
 (require 'cl)
 
 (defconst wb-base-url "http://www.cons.org/cmucl/")
-(defconst wb-doc-url  "ftp://ftp.linux.org.uk/pub/lisp/cmucl/doc/")
+;; (defconst wb-doc-url  "ftp://ftp.linux.org.uk/pub/lisp/cmucl/doc/")
+(defconst wb-doc-url  "http://common-lisp.net/project/cmucl/doc/")
 (defconst wb-target-dir "/project/cmucl/public_html/mirror/")
 (defconst wb-base-directory default-directory)
 

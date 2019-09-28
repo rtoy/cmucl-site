2f07b6e07e5d28bc70dda85cae418e23f4db0180
Raymond Toy <toy.raymond@gmail.com>
2019-09-28 14:47:59 -0700

    Use texinfo 6.7
    Version 6.7 has been released, so let's update the tree (if it exists)
    and build version 6.7.

--- build-texinfo.sh
+++ build-texinfo.sh
@@ -8,12 +8,13 @@ set -x
 if [ -d texinfo ]; then
    cd texinfo
    git stash; git stash drop
+   git pull --rebase
 else
    git clone https://git.savannah.gnu.org/git/texinfo.git
    cd texinfo
 fi
 # Check out the desired version
-git checkout 63d8c9102dcb454842330f7477f5c7e38b938aba
+git checkout texinfo-6.7
 
 # If the bin directory doesn't exist, assume we need to build and
 # install texinfo
